aa6e6dd2f5a1617c038259f11b320927cd6cbe1a
S.M. MOZAMMAL <mdimon1792@gmail.com>
2023-02-02 01:27:15 +0600

    commite 1

---
+++ lib/screen/onbording/emailverification_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EmailVarificationScreen extends StatefulWidget {
+  const EmailVarificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EmailVarificationScreen> createState() => _EmailVarificationScreenState();
+}
+
+class _EmailVarificationScreenState extends State<EmailVarificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
